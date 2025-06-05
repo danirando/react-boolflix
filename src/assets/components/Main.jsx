77f33b8be2b5032b10c8070d323764454daa54f7
danirando <danielerando@tiscali.it>
2025-06-05 13:53:30 +0200

    aggiungo img delle bandiere per le lingue dei film

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,18 +1,30 @@
 export default function Main({ results }) {
+  const supportedFlags = ["GB", "IT", "FR", "ES", "DE"];
   return (
     <>
-      {results.map((res) => (
-        <div key={res.id} className="continer">
-          <ul>
-            <li>
-              <h1>{res.title}</h1>
-              <h2>Titolo Originale: {res.original_title}</h2>
-              <h3>Lingua: {res.original_language}</h3>
-              <p>Voto: {res.vote_average}</p>
-            </li>
-          </ul>
-        </div>
-      ))}
+      {results.map((res) => {
+        const lang = res.original_language?.toLowerCase();
+        const countryCode = lang === "en" ? "gb" : lang;
+        return (
+          <div key={res.id} className="container">
+            <ul>
+              <li>
+                <h1>{res.title}</h1>
+                <h2>Titolo Originale: {res.original_title}</h2>
+                <img
+                  src={`https://flagsapi.com/${countryCode.toUpperCase()}/flat/64.png`}
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "/fallback.png";
+                  }}
+                  alt={`${countryCode} flag`}
+                />
+                <p>Voto: {res.vote_average}</p>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
     </>
   );
 }
